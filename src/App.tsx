79a9eaa88fd2e597737f0ad323ed98729d2ab653
Alexandr <alexryapolov36@gmail.com>
2022-05-05 17:49:03 +0400

    add custom router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header/Header';
 import { Home } from './routes/Home';
 import { Main } from './routes/Main';
 import { NotFound } from './routes/NotFound';
 import { Profile } from './routes/Profile';
 import './App.scss';
+import { Route } from './components/Route/Route';
 
 function App() {
   return (
-    <Router>
-      <div className="app">
-        <Header />
-        <div className="app__content">
-          <Routes>
-            <Route path="/main" element={<Main />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/" element={<Home />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
+    <div className="app">
+      <Header />
+      <div className="app__content">
+        <Route path="/main" element={<Main />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
       </div>
-    </Router>
+    </div>
   );
 }
 
